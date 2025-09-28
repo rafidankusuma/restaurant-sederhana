f4bd02bd3ed3973f1ba73a75238120f77cc0a36e
Rafidan Kusuma <EMAIL_GITHUB_KAMU@example.com>
2025-09-28 16:38:39 +0700

    first commit

--- app/menu/[foodId]/page.tsx
+++ app/menu/[foodId]/page.tsx
@@ -2,20 +2,20 @@ import { Metadata } from "next"
 import { Suspense } from "react"
 import FoodDetail from "@/components/food-detail"
 
-export const metadata:Metadata ={
-    title:"Food Detail"
+export const metadata: Metadata = {
+    title: "Food Detail"
 }
 
-const FoodDetailPage = async ({ params }: { params: { foodId: string } }) => {
-    const { foodId } = params; // destructure langsung
+const FoodDetailPage = ({ params }: { params: { foodId: string } }) => {
+    const { foodId } = params; 
     return (
         <div className="mt-16">
-        <Suspense fallback={<p>Loading...</p>}>
-            <FoodDetail foodId={foodId}/>
-        </Suspense>
-        
-    </div>
-  )
+            <Suspense fallback={<p>Loading...</p>}>
+                <FoodDetail foodId={foodId} />
+            </Suspense>
+
+        </div>
+    )
 }
 
 export default FoodDetailPage
