111707982e99ad2730d270ce2afaf5e3077c1071
Rafidan Kusuma <EMAIL_GITHUB_KAMU@example.com>
2025-09-29 14:26:13 +0700

    Update kode

--- components/hero.tsx
+++ components/hero.tsx
@@ -22,14 +22,14 @@ const Hero = () => {
                 </p>
                 <div className="flex gap-5">
                     <Link
-                        href="/contact"
+                        href="/menu"
                         className="bg-orange-400 text-white hover:bg-orange-500 py-2.5 
                         px-6 md:px-10 text-lg font-semibold hover:scale-105 hover:shadow-lg"
                     >
                         Order Now
                     </Link>
                     <Link
-                        href="/room"
+                        href="/contact"
                         className="bg-transparent border border-orange-400 text-white hover:bg-orange-500 
                         py-2.5 px-6 md:px-10 text-lg font-semibold hover:scale-105 hover:shadow-lg"
                     >
